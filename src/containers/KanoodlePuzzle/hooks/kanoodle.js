90189bf272639ac693add26ced56ab439b3a2168
sara.hasan <sara.hasan@nextgeni.com>
2023-11-14 15:17:36 +0000

    Make changes for jest config in package.json file

--- src/containers/KanoodlePuzzle/hooks/kanoodle.js
+++ src/containers/KanoodlePuzzle/hooks/kanoodle.js
@@ -128,7 +128,7 @@ export default function useKanoodle() {
 
         try {
             if (typeof (Worker) !== "undefined") {
-                WebWorker = new Worker(new URL('./worker/app.worker.js', import.meta.url));
+                WebWorker = new Worker(`${process.env.REACT_APP_PUBLIC_URL}/worker/app.worker.js`);
                 if (window.Worker) { 
                     // Web workers are supported
                     console.log("Web workers are supported")
